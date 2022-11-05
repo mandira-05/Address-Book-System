666d88acc7a62c10ebb1cb7abfed87053902517e
mandira-05 <mandira.sn.gowda@gmail.com>
2022-11-05 22:12:03 +0530

    Added to edit a contact,display all contacts

--- AddressBookProgram.java
+++ AddressBookProgram.java
@@ -1,58 +1,165 @@
 package addressbooksystem;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBookProgram {
-    public static void address() {
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter first name");
-        String firstName = scanner.next();
+        //Considered arraylist -name contactList
+        static ArrayList<ContactItems> contactList = new ArrayList<>();
 
-        System.out.println("Enter last name");
-        String lastName = scanner.next();
+        public void display(ArrayList < ContactItems > contactList) {
+            for (ContactItems contactItems : contactList) {
+                System.out.println(contactItems);
+            }
+        }
 
-        System.out.println("Enter Address");
-        String address = scanner.next();
+        //Method to add a contact
+        public void addContact(ContactItems contactItems, ArrayList <ContactItems> contactList){
 
-        System.out.println("Enter city");
-        String city = scanner.next();
+            contactItems = new ContactItems();
 
-        System.out.println("Enter state");
-        String state = scanner.next();
+            System.out.println("Enter first name");
+            contactItems.firstName = scanner.next();
 
-        System.out.println("Enter zip code");
-        String zipCode = scanner.next();
+            System.out.println("Enter last name");
+            contactItems.lastName = scanner.next();
 
-        System.out.println("Enter phone number");
-        long phoneNumber = scanner.nextLong();
+            System.out.println("Enter Address");
+            contactItems.address = scanner.next();
 
-        System.out.println("Enter Email");
-        String emailId = scanner.next();
+            System.out.println("Enter city");
+            contactItems.city = scanner.next();
 
-        System.out.println("First Name: " +firstName);
-        System.out.println("Last Name: " +lastName);
-        System.out.println("Address: " +address);
-        System.out.println("City: " +city);
-        System.out.println("State: " +state);
-        System.out.println("Zip Code: " +zipCode);
-        System.out.println("Phone number: " +phoneNumber);
-        System.out.println("Email: " +emailId);
-    }
+            System.out.println("Enter state");
+            contactItems.state = scanner.next();
 
-    public static void main(String[] args) {
-        //System.out.println("Welcome to Address Book");
-        //address();
+            System.out.println("Enter zip code");
+            contactItems.pinCode = scanner.nextInt();
 
-        AddressBookProgram contact1 = new AddressBookProgram();
-        System.out.println("Enter Details for contact1");
-        contact1.address();
+            System.out.println("Enter phone number");
+            contactItems.phoneNumber = scanner.nextLong();
 
-        AddressBookProgram contact2 = new AddressBookProgram();
-        System.out.println("Enter Details for contact2");
-        contact2.address();
+            System.out.println("Enter Email");
+            contactItems.email = scanner.next();
 
-    }
+           contactList.add(contactItems);
 
 
+        }
+
+        //Method to edit existing contact in address book
+        public void editContact() {
+            System.out.println("Enter the first name to edit contact");
+            String edit = scanner.next();
+            System.out.println("Press 1 to edit first name");
+            System.out.println("Press 2 to edit last name");
+            System.out.println("Press 3 to edit address");
+            System.out.println("Press 4 to edit city");
+            System.out.println("Press 5 to edit state");
+            System.out.println("Press 6 to edit pincode");
+            System.out.println("Press 7 to edit Phone number");
+            System.out.println("Press 8 to edit email");
+
+
+            int choice = scanner.nextInt();
+            if (choice == 1) {
+                for (ContactItems contactItems : contactList) {
+                    if (contactItems.firstName.equals(edit)) {
+                        System.out.println("Enter new first name");
+                        contactItems.firstName = scanner.next();
+                    }
+                }
+            }
+
+            if (choice == 2) {
+                for (ContactItems contactItems : contactList) {
+                    if (contactItems.firstName.equals(edit))  {
+                        System.out.println("Enter new last name");
+                        contactItems.lastName = scanner.next();
+                    }
+                }
+            }
+            if (choice == 3) {
+                for (ContactItems contactItems : contactList) {
+                    if (contactItems.firstName.equals(edit))  {
+                        System.out.println("Enter new address");
+                        contactItems.address = scanner.next();
+                    }
+                }
+            }
+
+            if (choice == 4) {
+                for (ContactItems contactItems : contactList) {
+                    if (contactItems.firstName.equals(edit))  {
+                        System.out.println("Enter new city");
+                        contactItems.city = scanner.next();
+                    }
+                }
+            }
+
+            if (choice == 5) {
+                for (ContactItems contactItems : contactList) {
+                    if (contactItems.firstName.equals(edit))  {
+                        System.out.println("Enter new city");
+                        contactItems.state = scanner.next();
+                    }
+                }
+            }
+
+            if (choice == 6) {
+                for (ContactItems contactItems : contactList) {
+                    if (contactItems.firstName.equals(edit))  {
+                        System.out.println("Enter new Pin code");
+                        contactItems.pinCode = scanner.nextInt();
+                    }
+                }
+            }
+            if (choice == 7) {
+                for (ContactItems contactItems : contactList) {
+                    if (contactItems.firstName.equals(edit))  {
+                        System.out.println("Enter new Phone number");
+                        contactItems.phoneNumber = scanner.nextLong();
+                    }
+                }
+            }
+            if (choice == 8) {
+                for (ContactItems contactItems : contactList) {
+                    if (contactItems.firstName.equals(edit))  {
+                        System.out.println("Enter new email");
+                        contactItems.email = scanner.next();
+                    }
+                }
+            }
+        }
+
+        public void addContactList() {
+            while (true) {   ///should run always
+                System.out.println("Press 0 - Display all contacts");
+                System.out.println("Press 1 - Add contact");
+                System.out.println("Press 2 - Edit contact");
+                System.out.println("Press 3 - Exit");
+
+
+                int option= scanner.nextInt();
+
+                switch (option) {
+                    case 0:
+                        display(contactList);
+                        break;
+                    case 1:
+                        addContact(null,contactList);
+                        break;
+                    case 2:
+                        editContact();
+                        break;
+
+                }
+                if (option == 3) {
+                    break;
+                }
+            }
+        }
 }
---
+++ ContactItems.java
@@ -0,0 +1,82 @@
+package addressbooksystem;
+
+public class ContactItems {
+     String firstName;
+     String lastName;
+     String address;
+     String city;
+     String state;
+     int pinCode;
+     long phoneNumber;
+     String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(int pinCode) {
+        this.pinCode = pinCode;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String toString () {
+        return "ContactItems [First Name: " +firstName+ ", Last Name : " +lastName+
+        ", address : " +address+ ", city: " +city+ ",state : " +state+ ",Pincode : "
+        +pinCode+ ",Phone Number: " +phoneNumber+ ", Email : " +email+ "]";
+    }
+
+}
